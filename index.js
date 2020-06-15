1e555de06d11432d0776a60dc5befad1a9ae16e9
kosinix <kosinix@users.noreply.github.com>
2020-06-15 19:02:11 +0800

    Add barangays

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const regions = require('./regions');
 const provinces = require('./provinces');
 const citiesMuns = require('./cities-muns');
 const citiesNcr = require('./cities-ncr');
+const barangays = require('./barangays');
 const groupedProvincesByRegion = lodash.groupBy(provinces, (o) => {
     return o.regCode;
 });
 const groupedCitiesMunsByProvince = lodash.groupBy(citiesMuns, (o) => {
     return o.provCode;
+});
+const groupedBarangaysByCitiesMuns = lodash.groupBy(barangays, (o) => {
+    return o.cityMunCode;
 });
 
 module.exports = {
@@ -20,6 +24,8 @@ module.exports = {
     provinces: provinces,
     citiesMuns: citiesMuns,
     citiesNcr: citiesNcr,
+    barangays: barangays,
     groupedProvincesByRegion: groupedProvincesByRegion,
     groupedCitiesMunsByProvince: groupedCitiesMunsByProvince,
+    groupedBarangaysByCitiesMuns: groupedBarangaysByCitiesMuns,
 }
